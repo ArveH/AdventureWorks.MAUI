35c3c5b3a5f61f3f3ec5dbfb5f99a109b341f3c5
Arve Hansen <arveh4@outlook.com>
2024-01-20 21:59:25 +0100

    Added Switch Changed from On/Off -> Yes/No

--- AdventureWorks.MAUI/MauiProgram.cs
+++ AdventureWorks.MAUI/MauiProgram.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 
+#if WINDOWS
+using Microsoft.Maui.LifecycleEvents;
+#endif
+
 namespace AdventureWorks.MAUI
 {
     public static class MauiProgram
@@ -15,11 +19,56 @@ namespace AdventureWorks.MAUI
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+#if WINDOWS
+      //SetWindowOptions(builder);
+      SetWindowHandlers();
+#endif
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
+
+#if WINDOWS
+        public static void SetWindowOptions(MauiAppBuilder builder)
+        {
+          builder.ConfigureLifecycleEvents(events =>
+          {
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+              wndLifeCycleBuilder.OnWindowCreated(window =>
+              {
+                IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                Microsoft.UI.WindowId win32WindowsId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                Microsoft.UI.Windowing.AppWindow winuiAppWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(win32WindowsId);
+                if (winuiAppWindow.Presenter is Microsoft.UI.Windowing.OverlappedPresenter p) {
+                  p.Maximize();
+                  //p.IsResizable = false;
+                  //p.IsMaximizable = false;
+                  //p.IsMinimizable = false;
+                }
+              });
+            });
+          });
+        }
+#endif
+
+#if WINDOWS
+        public static void SetWindowHandlers()
+        {
+          Microsoft.Maui.Handlers.SwitchHandler.Mapper
+            .AppendToMapping("Custom", (h, v) =>
+          {
+            // Get rid of On/Off label beside switch, to match other platforms
+            h.PlatformView.OffContent = "No";
+            h.PlatformView.OnContent = "Yes";
+
+            h.PlatformView.MinWidth = 0;
+          });
+        }
+#endif
+
     }
 }
