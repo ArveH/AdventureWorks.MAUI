5cfdfc9cf956e7fe61802ddd07f2ffe5ab6ad38c
Arve Hansen <arveh4@outlook.com>
2024-01-20 22:43:29 +0100

    Created converter

---
+++ AdventureWorks.MAUI/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace AdventureWorks.MAUI.Converters;
+
+public class InvertedBoolConverter : IValueConverter
+{
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    return !(bool)(value ?? true);
+  }
+
+  public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    return null;
+  }
+}
