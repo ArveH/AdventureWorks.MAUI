f70d3999372c8a7e24b06b3d49ba04ce51459a2d
Arve Hansen <arveh4@outlook.com>
2024-01-21 09:51:57 +0100

    Added Common project Using OnPropertyChanged

--- AdventureWorks.EntityLayer/EntityClasses/User.cs
+++ AdventureWorks.EntityLayer/EntityClasses/User.cs
@@ -1,21 +1,197 @@
-﻿namespace AdventureWorks.EntityLayer;
+﻿using Common.Library;
 
-public class User
+namespace AdventureWorks.EntityLayer;
+
+public class User : EntityBase
 {
-    public DateTime BirthDate { get; set; }
-    public string Email { get; set; }
-    public string FirstName { get; set; }
-    public bool IsActive{ get; set; }
-    public bool IsEnrolledIn401k{ get; set; }
-    public bool IsEnrolledInFlexTime{ get; set; }
-    public bool IsEnrolledInHealthCare{ get; set; }
-    public bool IsEnrolledInHSA{ get; set; }
-    public bool IsFullTime{ get; set; }
-    public string LastName { get; set; }
-    public string LoginId { get; set; }
-    public string Password { get; set; }
-    public string Phone { get; set; }
-    public string PhoneType { get; set; }
-    public TimeSpan StartTime { get; set; }
-    public int UserId { get; set; }
+    #region Private Variables
+    private int _UserId;
+    private string _LoginId = string.Empty;
+    private string _FirstName = string.Empty;
+    private string _LastName = string.Empty;
+    private string _Email = string.Empty;
+    private string _Password = string.Empty;
+    private string _Phone = string.Empty;
+    private string _PhoneType = string.Empty;
+    private bool _IsFullTime;
+    private bool _IsEnrolledIn401k;
+    private bool _IsEnrolledInHealthCare;
+    private bool _IsEnrolledInHSA;
+    private bool _IsEnrolledInFlexTime;
+    private bool _IsActive;
+    private DateTime _BirthDate = new();
+    private TimeSpan _StartTime = new(6, 0, 0);
+    #endregion
+
+    #region Public Properties
+    public int UserId
+    {
+        get { return _UserId; }
+        set
+        {
+            _UserId = value;
+            RaisePropertyChanged(nameof(UserId));
+        }
+    }
+
+    public string LoginId
+    {
+        get { return _LoginId; }
+        set
+        {
+            _LoginId = value;
+            RaisePropertyChanged(nameof(LoginId));
+        }
+    }
+
+    public string FirstName
+    {
+        get { return _FirstName; }
+        set
+        {
+            _FirstName = value;
+            RaisePropertyChanged(nameof(FirstName));
+        }
+    }
+
+    public string LastName
+    {
+        get { return _LastName; }
+        set
+        {
+            _LastName = value;
+            RaisePropertyChanged(nameof(LastName));
+        }
+    }
+
+    public string Email
+    {
+        get { return _Email; }
+        set
+        {
+            _Email = value;
+            RaisePropertyChanged(nameof(Email));
+        }
+    }
+
+    public string Password
+    {
+        get { return _Password; }
+        set
+        {
+            _Password = value;
+            RaisePropertyChanged(nameof(Password));
+        }
+    }
+
+    public string Phone
+    {
+        get { return _Phone; }
+        set
+        {
+            _Phone = value;
+            RaisePropertyChanged(nameof(Phone));
+        }
+    }
+
+    public string PhoneType
+    {
+        get { return _PhoneType; }
+        set
+        {
+            _PhoneType = value;
+            RaisePropertyChanged(nameof(PhoneType));
+        }
+    }
+
+    public bool IsFullTime
+    {
+        get { return _IsFullTime; }
+        set
+        {
+            _IsFullTime = value;
+            RaisePropertyChanged(nameof(IsFullTime));
+        }
+    }
+
+    public bool IsEnrolledIn401k
+    {
+        get { return _IsEnrolledIn401k; }
+        set
+        {
+            _IsEnrolledIn401k = value;
+            RaisePropertyChanged(nameof(IsEnrolledIn401k));
+        }
+    }
+
+    public bool IsEnrolledInHealthCare
+    {
+        get { return _IsEnrolledInHealthCare; }
+        set
+        {
+            _IsEnrolledInHealthCare = value;
+            RaisePropertyChanged(nameof(IsEnrolledInHealthCare));
+        }
+    }
+
+    public bool IsEnrolledInHSA
+    {
+        get { return _IsEnrolledInHSA; }
+        set
+        {
+            _IsEnrolledInHSA = value;
+            RaisePropertyChanged(nameof(IsEnrolledInHSA));
+        }
+    }
+
+    public bool IsEnrolledInFlexTime
+    {
+        get { return _IsEnrolledInFlexTime; }
+        set
+        {
+            _IsEnrolledInFlexTime = value;
+            RaisePropertyChanged(nameof(IsEnrolledInFlexTime));
+        }
+    }
+
+    public bool IsActive
+    {
+        get { return _IsActive; }
+        set
+        {
+            _IsActive = value;
+            RaisePropertyChanged(nameof(IsActive));
+        }
+    }
+
+    public DateTime BirthDate
+    {
+        get { return _BirthDate; }
+        set
+        {
+            _BirthDate = value;
+            RaisePropertyChanged(nameof(BirthDate));
+        }
+    }
+
+    public TimeSpan StartTime
+    {
+        get { return _StartTime; }
+        set
+        {
+            _StartTime = value;
+            RaisePropertyChanged(nameof(StartTime));
+        }
+    }
+
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+    }
+
+    public string LastNameFirstName
+    {
+        get { return LastName + ", " + FirstName; }
+    }
+    #endregion
 }
--- AdventureWorks.MAUI/Views/UserDetailView.xaml.cs
+++ AdventureWorks.MAUI/Views/UserDetailView.xaml.cs
@@ -1,3 +1,5 @@
+using AdventureWorks.EntityLayer;
+
 namespace AdventureWorks.MAUI.Views;
 
 public partial class UserDetailView : ContentPage
@@ -5,5 +7,23 @@ public partial class UserDetailView : ContentPage
 	public UserDetailView()
 	{
 		InitializeComponent();
-	}
+
+        UserObject = (User)this.Resources["ViewModel"];
+    }
+
+	public User UserObject { get; set; }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        UserObject.LoginId = "PeterPiper384";
+        UserObject.FirstName = "Peter";
+        UserObject.LastName = "Piper";
+        UserObject.Email = "Peter@Pipering.com";
+    }
+
+    private void SaveButton_Clicked(object? sender, EventArgs e)
+    {
+        System.Diagnostics.Debugger.Break();
+    }
 }
